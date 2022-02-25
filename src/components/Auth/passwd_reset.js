6037772b2ae32156518ccbc9e867ddf79af71192
RAMAN DWIVEDI <ramandwivedi20@protonmail.com>
2022-02-25 18:09:02 +0530

    Update passwd_reset.js

--- src/components/Auth/passwd_reset.js
+++ src/components/Auth/passwd_reset.js
@@ -33,7 +33,7 @@ export default class Login extends React.Component {
             <div className='login'>
                 <form onSubmit={this.handleSubmit}>
                     <input type='password' value={this.state.password} name='password' onChange={this.handleChange} />
-                    <input type='password2' value={this.state.password2} name='password2' onChange={this.handleChange} />
+                    <input type='text' value={this.state.password2} name='password2' onChange={this.handleChange} />
                     <input type='submit' />
                 </form>
                 <span>login</span>
